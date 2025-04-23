0e916bff90dcfae24e4d12871f6ac9a82c003bad
GitJohnFis <jfisiiahi69@gmail.com>
2025-04-23 19:43:30 +1200

    Create basic counter in svelte

---
+++ counterApp.svelte
@@ -0,0 +1,102 @@
+<script>
+//create reactive state
+let count = 0;
+</script>
+
+<button on:click={() => count++}>
+count is {count}
+</button>
+
+/*
+
+//initialize code
+<script>
+onMount(async () => {
+	//handle promise
+});
+</script>
+
+//side-effect
+<script>
+let count;
+
+
+$: document.title = `count is ${count}`
+</script>
+
+//create computated state
+<script>
+let count;
+
+$: doubled = count * 2
+$: document.title = `count is ${count}`
+</script>
+
+//conditional logic
+
+<script>
+	let count = 0;
+</script>
+
+{#if count > 1000}
+	<p>big</p>
+{:else if count > 500}
+	<p>medium</p>
+{:else}
+	<p>small</p>
+{/f}
+
+
+//loops
+<script>
+	const items = [
+		{ id: 1, name: "foo" },
+		{ id: 2, name: "bar" },
+		{ id: 3, name: "ball" },
+	]
+</script>
+
+{#each items as item (item.id)}
+	<p>{item.name}</p>
+{/each}
+
+
+//shared state
+
+//in store.js file
+import { writable } from "svelte/store";
+
+export const countStore = writable(0);
+
+
+<script>
+	import { countStore } from "./store"
+</script>
+
+<button on:click={() => countStore.update(c => c + 1)}>
+count is {$countStore}
+</button>
+
+//features
+//surgical precision
+//zero boilerplate
+//bonus* automatic disposal
+
+//promises
+
+<script>
+	import {LoadingSpinner, ErrorPage} from
+
+	const promise = Promise.resolve(420);
+</script>
+
+{#await promise}
+	<LoadingSpinner />
+{:then number}
+ <p>The number is {number}</p>
+{:catch error}
+ <ErrorPage {error} />
+{/await}
+
+
+*/
