04ffdce1473151c5d9ad4952909940bdf672f7e4
GitJohnFis <jfisiiahi69@gmail.com>
2025-04-29 14:23:37 +1200

    update store.js with the new cards, sum, and message variables
    - working functionality for secret mode stored in house

--- store.js
+++ store.js
@@ -7,3 +7,8 @@ export const doubleClickStore = writable(false);
 //color theme
 export const activeTab = writable('counter');
 export const themeColor = writable('#007bff');
+//list of cards
+export const cards = writable([]);
+export const revealedCards = writable([]); // Whether each card has been revealed (true/false for each card)
+export const sum = writable(0); // Sum of revealed card values
+export const message = writable(''); // Message to the player ("You win!", "Bust!", etc.)
